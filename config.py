2ac445508290c5570a75da2d8a8fd4861a05d8f3
mrm15 <mrm.learn@gmail.com>
2024-02-22 00:49:23 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+source_directory = "C:/Users/mohamad/Desktop/test"
+destination_base = "C:/Users/mohamad/Desktop/test"
+destination_folders = ["امیرحسین", "علی میرعرب", "محمد میرعرب"]  # Update destination folders
+log_file = "custom_move_log.txt"  # Update log file name
